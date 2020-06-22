58c39697ad74afcdae3f092ef8027f6595bde4e2
furabonos <readyordie3974@gmail.com>
2020-06-22 21:28:23 +0900

    setting

---
+++ FlowTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlowTest
+//
+//  Created by 엄태형 on 2020/06/22.
+//  Copyright © 2020 엄태형. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FlowTestTests/FlowTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  FlowTestTests.swift
+//  FlowTestTests
+//
+//  Created by 엄태형 on 2020/06/22.
+//  Copyright © 2020 엄태형. All rights reserved.
+//
+
+import XCTest
+@testable import FlowTest
+
+class FlowTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
